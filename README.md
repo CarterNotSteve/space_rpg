8e86ffc1fd6b0421daa8ad1a32f0a3a6256da1af
CarterNotSteve <carterjw2005@gmail.com>
2018-03-24 19:29:42 -0700

    *small formatting change *still does pretty much nothing...

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a program I'm writing to learn `python` and `pygame`.
 ## Change log
  * v0.0.1 
  Will be awesome.
+ 
+ * v0.0.2
+ small code formatting changes.
+ still does basically nothing.
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import pygame
-import random
+import pygame, sys, random
 
-playerlevel = 0
+playerlevel = 1
 exp = 0
+
+
+def kill():
+    global exp
+    exp = exp + 100
+    if exp == 1000:
+        global playerlevel
+        playerlevel = playerlevel + 1
+
+def print_exp():
+    print("Player level %d (%d exp)" % (playerlevel, exp))
+
+if exp == 1000:
+    playerlevel = playerlevel + 1
+
+
+
